e1c0d8a95ff584c04b217808dc49f0533b9acdd3
Nazfib <mrev1995@gmail.com>
2024-05-09 00:22:57 +0200

    Update create-release.sh
    LICENSE.MechJebLib doesn't exist anymore

--- create-release.sh
+++ create-release.sh
@@ -10,7 +10,7 @@ cd "release/"
 
 cp -r "../GameData" "TransferWindowPlanner2/"
 cp "../TransferWindowPlanner2/bin/Release/net48/TransferWindowPlanner2.dll" "TransferWindowPlanner2/GameData/TransferWindowPlanner2"
-cp "../LICENSE" "../LICENSE.TransferWindowPlanner" "../LICENSE.MechJebLib" "../LICENSE.ClickThroughBlocker" "TransferWindowPlanner2/"
+cp "../LICENSE" "../LICENSE.TransferWindowPlanner" "../LICENSE.ClickThroughBlocker" "TransferWindowPlanner2/"
 cp "../README.md" "TransferWindowPlanner2/README.md"
 
 zip -FS -r "TransferWindowPlanner2_v${VERSION}.zip" "TransferWindowPlanner2"
