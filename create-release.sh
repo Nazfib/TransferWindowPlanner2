7d0c097135bb346391dfcb4718401eb2a99056ac
Nazfib <mrev1995@gmail.com>
2026-04-22 01:06:14 +0200

    Version 0.4.0

--- create-release.sh
+++ create-release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eux
 
-VERSION="0.3.0-preview"
+VERSION="0.4.0"
 
 dotnet build --configuration Release
 
@@ -9,9 +9,18 @@ rm -rf "release/TransferWindowPlanner2"
 mkdir -p "release/TransferWindowPlanner2"
 cd "release/"
 
-cp -r "../GameData" "TransferWindowPlanner2/"
-cp "../TransferWindowPlanner2/bin/Release/net48/TransferWindowPlanner2.dll" "TransferWindowPlanner2/GameData/TransferWindowPlanner2"
-cp "../LICENSE" "../LICENSE.TransferWindowPlanner" "../LICENSE.ClickThroughBlocker" "TransferWindowPlanner2/"
-cp "../README.md" "TransferWindowPlanner2/README.md"
+FILENAME="TransferWindowPlanner2_v${VERSION}.zip"
+if [ -f "$FILENAME" ]; then
+  echo "Zip file already exists!"
+  exit 1
+fi
 
-zip -FS -r "TransferWindowPlanner2_v${VERSION}.zip" "TransferWindowPlanner2"
+cp -r "../GameData" \
+  "../LICENSE" \
+  "../LICENSE.ClickThroughBlocker" \
+  "../LICENSE.TransferWindowPlanner" \
+  "../README.md" \
+  "TransferWindowPlanner2/"
+
+zip -r "TransferWindowPlanner2_v${VERSION}.zip" "TransferWindowPlanner2"
+
