46a42f96968f804ec99dc16891a1158434f53139
Nazfib <mrev1995@gmail.com>
2026-04-22 00:13:47 +0200

    Create manual AssemblyInfo.cs

---
+++ TransferWindowPlanner2/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("TransferWindowPlanner2")]
+
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
+
+[assembly: KSPAssembly("TransferWindowPlanner2", 0, 3, 0)]
+[assembly: KSPAssemblyDependency("ClickThroughBlocker", 1, 0)]
