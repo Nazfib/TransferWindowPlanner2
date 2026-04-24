5937e8cda38690e0692d3e92f687f570bbcf10ca
Nazfib <mrev1995@gmail.com>
2026-04-24 04:18:39 +0200

    Update create-release.sh
    Put the GameData folder in the root of the zip file.

--- create-release.sh
+++ create-release.sh
@@ -5,22 +5,17 @@ VERSION="0.4.0"
 
 dotnet build --configuration Release
 
-rm -rf "release/TransferWindowPlanner2"
-mkdir -p "release/TransferWindowPlanner2"
-cd "release/"
-
 FILENAME="TransferWindowPlanner2_v${VERSION}.zip"
-if [ -f "$FILENAME" ]; then
+if [ -f "release/$FILENAME" ]; then
   echo "Zip file already exists!"
   exit 1
 fi
 
-cp -r "../GameData" \
-  "../LICENSE" \
-  "../LICENSE.ClickThroughBlocker" \
-  "../LICENSE.TransferWindowPlanner" \
-  "../README.md" \
-  "TransferWindowPlanner2/"
-
-zip -r "TransferWindowPlanner2_v${VERSION}.zip" "TransferWindowPlanner2"
+zip -r "$FILENAME" \
+  "GameData" \
+  "LICENSE" \
+  "LICENSE.ClickThroughBlocker" \
+  "LICENSE.TransferWindowPlanner" \
+  "README.md"
+mv "$FILENAME" release
 
