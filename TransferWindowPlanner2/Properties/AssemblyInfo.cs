97921cb16b0200e85d61f6caea6b06afd6f2c2df
Nazfib <mrev1995@gmail.com>
2026-06-02 14:16:28 +0200

    v0.5.1

--- TransferWindowPlanner2/Properties/AssemblyInfo.cs
+++ TransferWindowPlanner2/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("TransferWindowPlanner2")]
 
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
 
 [assembly: KSPAssembly("TransferWindowPlanner2", 0, 5, 0)]
 [assembly: KSPAssemblyDependency("ClickThroughBlocker", 1, 0)]
